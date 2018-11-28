38c8e2bd7261783cb2ccbfc159a357b6756c7c93
EliSparow <max.pp@hotmail.fr>
2018-11-29 00:15:02 +0100

    integration view_index with products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 		get '/', to: 'template#index'
-		get '/shop/filter', to: 'template#shop_filter'
+		get '/shop', to: 'template#shop_full'
 		get '/about', to: 'template#about'
 		get '/single/:id', to: 'template#showproduct'
 		get '/single', to: 'template#showproduct'
