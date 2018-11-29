7944080dc6dd6b59780c2d3e4336c7e105d0fd3b
Max <43542850+EliSparow@users.noreply.github.com>
2018-11-29 15:48:45 +0100

    Revert "call to action fonctional"

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,29 +1,7 @@
 class TemplateController < ApplicationController
-	# after_checkout :send_email_to_admin
-
-
 
 	def index
 		@item = Item.all
-	end
-
-	def call_to_action
-		@visiter = Visiter.new
-		@visiter.email = params["email"]
-    @visiter.save
-
-    respond_to do |format|
-      if @visiter.save
-        # Tell the UserMailer to send a welcome email after save
-        UserMailer.with(visiter: @visiter).confirmation_order.deliver_now
-
-        format.html { redirect_to("/") }
-        format.json { render json: @visiter, status: :created, location: @visiter }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @visiter.errors, status: :unprocessable_entity }
-      end
-    end
 	end
 
 	def about
@@ -44,25 +22,5 @@ class TemplateController < ApplicationController
 	def show_test
 	  @individual = Item.find(params[:id])
 	end
-
-	# def send_email_to_admin
-	# 	@user = User.new
-	#   @user.email = params["email"]
-	#   @user.save
-
-
-	#   respond_to do |format|
-	#     if @user.save
-	#       # Tell the UserMailer to send a welcome email after save
-	#       UserMailer.with(user: @user).confirmation_email.deliver_now
-
-	#       format.html { redirect_to(root_path) }
-	#       format.json { render json: @user, status: :created, location: @user }
-	#     # else
-	#     #   format.html { render action: 'new' }
-	#     #   format.json { render json: @user.errors, status: :unprocessable_entity }
-	#     end
-	#   end
-	# end
 
 end
--- db/migrate/20181129115946_create_visiters.rb
+++
@@ -1,9 +0,0 @@
-class CreateVisiters < ActiveRecord::Migration[5.2]
-  def change
-    create_table :visiters do |t|
-      t.string :email
-
-      t.timestamps
-    end
-  end
-end
